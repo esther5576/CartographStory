7d773aaf4e33b0a24a790ff90a5197af5e219cd8
esther5576 <esther.berges@hotmail.com>
2018-05-07 13:56:33 +0200

    Boat Controls
    Code des contrôles du bateau

---
+++ Assets/Scripts/BoatController/BoatNavigation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatNavigation: MonoBehaviour
+{
+    public float turnSpeed = 10000f;
+    public float accelerationSpeed = 200000;
+
+    private Rigidbody boatRigibody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        boatRigibody = this.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        boatRigibody.AddTorque(0f, h * turnSpeed * Time.deltaTime, 0f);
+        boatRigibody.AddForce(transform.forward * v * accelerationSpeed * Time.deltaTime);
+	}
+}
