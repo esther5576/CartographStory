c4b6041ac980ff24d44694e55501a6207bbe3bfc
Esther <esther.berges@hotmail.com>
2018-05-07 22:32:40 +0200

    Boat navigation

--- Assets/Scripts/BOAT_Controller/BoatEngine.cs
+++ Assets/Scripts/BOAT_Controller/BoatEngine.cs
@@ -4,8 +4,52 @@ using UnityEngine;
 
 public class BoatEngine : MonoBehaviour
 {
+    private float WaterJetRotation_Y = 0;
+    public Transform waterJetTransform;
+    public float accelerationSpeed = 200000;
+
+    private Rigidbody boatRigibody;
+
+    // Use this for initialization
+    void Start()
+    {
+        boatRigibody = this.GetComponent<Rigidbody>();
+    }
+
+
     public void Update()
     {
-        
+        //Steer left
+        if (Input.GetKey(KeyCode.Q))
+        {
+            WaterJetRotation_Y = waterJetTransform.localEulerAngles.y + 2f;
+
+            if (WaterJetRotation_Y > 30f && WaterJetRotation_Y < 270f)
+            {
+                WaterJetRotation_Y = 30f;
+            }
+
+            Vector3 newRotation = new Vector3(0f, WaterJetRotation_Y, 0f);
+
+            waterJetTransform.localEulerAngles = newRotation;
+        }
+        //Steer right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            WaterJetRotation_Y = waterJetTransform.localEulerAngles.y - 2f;
+
+            if (WaterJetRotation_Y < 330f && WaterJetRotation_Y > 90f)
+            {
+                WaterJetRotation_Y = 330f;
+            }
+
+            Vector3 newRotation = new Vector3(0f, WaterJetRotation_Y, 0f);
+
+            waterJetTransform.localEulerAngles = newRotation;
+
+
+            float v = Input.GetAxis("Vertical");
+            boatRigibody.AddForce(waterJetTransform.forward * v * accelerationSpeed * Time.deltaTime);
+        }
     }
 }
