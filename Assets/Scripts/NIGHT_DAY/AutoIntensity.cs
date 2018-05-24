2eb1b67b8c2dfec778e3b4c225638516fd05696b
esther5576 <esther.berges@hotmail.com>
2018-05-24 08:53:04 +0200

    fixed night

---
+++ Assets/Scripts/NIGHT_DAY/AutoIntensity.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoIntensity : MonoBehaviour {
+    [Header("Sun and sky settings --------------------")]
+	public Gradient nightDayColor;
+
+	public float maxIntensity = 3f;
+	public float minIntensity = 0f;
+	public float minPoint = -0.2f;
+
+	public float maxAmbient = 1f;
+	public float minAmbient = 0f;
+	public float minAmbientPoint = -0.2f;
+
+
+	public Gradient nightDayFogColor;
+	public AnimationCurve fogDensityCurve;
+	public float fogScale = 1f;
+
+	public float dayAtmosphereThickness = 0.4f;
+	public float nightAtmosphereThickness = 0.87f;
+
+	public Vector3 dayRotateSpeed;
+	public Vector3 nightRotateSpeed;
+
+	float skySpeed = 1;
+
+
+	Light mainLight;
+	Skybox sky;
+	Material skyMat;
+
+    [Space(20)]
+
+    [Header("Boat Lights -----------------------------")]
+    public Light BoatLight;
+    public float BoatSpeed = 1;
+    public float BoatLightIntensity = 5;
+
+	void Start () 
+	{
+	
+		mainLight = GetComponent<Light>();
+		skyMat = RenderSettings.skybox;
+
+	}
+
+	void Update () 
+	{
+	
+		float tRange = 1 - minPoint;
+		float dot = Mathf.Clamp01 ((Vector3.Dot (mainLight.transform.forward, Vector3.down) - minPoint) / tRange);
+		float i = ((maxIntensity - minIntensity) * dot) + minIntensity;
+
+		mainLight.intensity = i;
+
+		tRange = 1 - minAmbientPoint;
+		dot = Mathf.Clamp01 ((Vector3.Dot (mainLight.transform.forward, Vector3.down) - minAmbientPoint) / tRange);
+		i = ((maxAmbient - minAmbient) * dot) + minAmbient;
+		RenderSettings.ambientIntensity = i;
+
+		mainLight.color = nightDayColor.Evaluate(dot);
+		RenderSettings.ambientLight = mainLight.color;
+
+		RenderSettings.fogColor = nightDayFogColor.Evaluate(dot);
+		RenderSettings.fogDensity = fogDensityCurve.Evaluate(dot) * fogScale;
+
+		i = ((dayAtmosphereThickness - nightAtmosphereThickness) * dot) + nightAtmosphereThickness;
+		skyMat.SetFloat ("_AtmosphereThickness", i);
+
+        if (dot > 0)
+        {
+            transform.Rotate(dayRotateSpeed * Time.deltaTime * skySpeed);
+            if (BoatLight.intensity > 0f)
+            {
+                BoatLight.intensity -= BoatSpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            transform.Rotate(nightRotateSpeed * Time.deltaTime * skySpeed);
+            if (BoatLight.intensity < BoatLightIntensity)
+            {
+                BoatLight.intensity += BoatSpeed * Time.deltaTime;
+            }
+        }
+	}
+}
