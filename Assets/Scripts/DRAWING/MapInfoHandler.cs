8dba319bc1dcb6f329267041f6833f29000fcf69
Kurtn <Kurtn77@gmail.com>
2018-05-25 23:34:03 +0200

    Map system
    r to draw new map
    M to open and close map
    Right click on map to edit

---
+++ Assets/Scripts/DRAWING/MapInfoHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MapInfoHandler : MonoBehaviour, IPointerDownHandler
+{
+	public MapManager ManagerScript;
+	public int ID;
+	public Image MapSprite;
+	private bool DragOn;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (MapSprite == null) MapSprite = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (DragOn)
+		{
+			transform.position = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			DragOn = false;
+		}
+	}
+	
+	public void OnPointerDown(PointerEventData data)
+	{
+		if (Input.GetMouseButton(0))
+		{
+			DragOn = true;
+		}
+		else
+		{
+			ManagerScript.StartDraw(ID);
+		}
+	}
+}
--- Assets/Scripts/DRAWING/MapManager.cs
+++ Assets/Scripts/DRAWING/MapManager.cs
@@ -1,16 +1,117 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
+	public List<Sprite> AllMapTextures;
+	public List<MapInfoHandler> AllMapScripts;
+
+	public int MapsUsed;
+
+	public Drawable DrawManager;
+
+	public Transform MapContainer;
+	
+	[Header("Buttons")]
+	public Button ResetButton;
+	public Button CloseBigMapButton;
+	public Button CloseDrawButton;
+	public Button MapValidationButton;
+
+
+	[Header("GameObjects")]
+	public GameObject DrawSystemParent;
+	public GameObject BigMapParent;
+	public GameObject MapPrefab;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+	{
+		ResetButton.onClick.AddListener(ResetDraw);
+		CloseBigMapButton.onClick.AddListener(CloseMap);
+		CloseDrawButton.onClick.AddListener(CloseDraw);
+		MapValidationButton.onClick.AddListener(ValidateDraw);
+		DrawManager.Reset_Canvas_On_Play = false;
+		DrawManager.CanDraw = false;
+		MapsUsed = 0;
 	}
-	
+
+	void ResetDraw ()
+	{
+		DrawManager.ResetCanvas();
+	}
+
+	public void CreateNewDraw ()
+	{
+		if (MapsUsed < AllMapTextures.Count - 1)
+		{
+			StartDraw(MapsUsed);
+			GameObject NewMap = Instantiate(MapPrefab, MapContainer);
+			AllMapScripts.Add(NewMap.GetComponent<MapInfoHandler>());
+			AllMapScripts[MapsUsed].ID = MapsUsed;
+			AllMapScripts[MapsUsed].ManagerScript = this;
+			AllMapScripts[MapsUsed].MapSprite.sprite = AllMapTextures[MapsUsed];
+			MapsUsed++;
+			DrawManager.ResetCanvas();
+			CloseDrawButton.interactable = false;
+		}
+		else
+		{
+			Debug.Log("No more map avaible");
+		}
+	}
+
+	public void StartDraw (int ID)
+	{
+		DrawManager.InitDrawer(AllMapTextures[ID]);
+		DrawManager.CanDraw = true;
+		OpenDraw();
+		CloseMap();
+	}
+
+	public void ValidateDraw ()
+	{
+		CloseDrawButton.interactable = true;
+		DrawManager.CanDraw = false;
+		OpenMap();
+		CloseDraw();
+	}
+
+	public void OpenDraw ()
+	{
+		DrawSystemParent.SetActive(true);
+	}
+
+	public void CloseDraw ()
+	{
+		DrawSystemParent.SetActive(false);
+	}
+
+	public void OpenMap ()
+	{
+		BigMapParent.SetActive(true);
+	}
+
+	public void CloseMap ()
+	{
+		BigMapParent.SetActive(false);
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			CreateNewDraw();
+		}
+
+		if (Input.GetKeyDown(KeyCode.M))
+		{
+			if (BigMapParent.activeInHierarchy) CloseMap();
+			else OpenMap();
+		}
 	}
 }
