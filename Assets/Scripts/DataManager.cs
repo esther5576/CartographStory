00e8f1a93d71f6918ead66a717b79aa8fe3b095f
Kurtn <Kurtn77@gmail.com>
2018-06-12 12:36:42 +0200

    Datamanager

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataManager
+{
+    static List<Island> AllIslands = new List<Island>();
+}
+
+public class Island
+{
+    Sprite Drawing;
+    List<Sprite> Pictures = new List<Sprite>();
+    string Text = "Aucun texte chargé";
+}
