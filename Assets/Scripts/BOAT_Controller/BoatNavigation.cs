76f0edda7aecd990df10cc0fda2a08ebb89373b5
esther5576 <esther.berges@hotmail.com>
2018-06-12 11:23:37 +0200

    Boat intégré + rotation desvoiles dynamique

--- Assets/Scripts/BOAT_Controller/BoatNavigation.cs
+++ Assets/Scripts/BOAT_Controller/BoatNavigation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BoatNavigation: MonoBehaviour
 {
@@ -11,8 +12,13 @@ public class BoatNavigation: MonoBehaviour
 
     private Rigidbody boatRigibody;
 
-	// Use this for initialization
-	void Start ()
+    public List<Transform> _l_partsToTurn;
+    public Vector3 rightTurn = new Vector3(0,-40,0);
+    public Vector3 leftTurn = new Vector3(0, 40, 0);
+    public float voileSpeedRot = 1;
+
+    // Use this for initialization
+    void Start ()
     {
         boatRigibody = this.GetComponent<Rigidbody>();
     }
@@ -27,5 +33,33 @@ public class BoatNavigation: MonoBehaviour
         boatRigibody.AddForce(transform.forward * v * accelerationSpeed * Time.deltaTime);
 
         //Debug.Log(boatRigibody.velocity.magnitude);
+
+        if (h > 0)
+        {
+            for (int i = 0; i < _l_partsToTurn.Count; i++)
+            {
+                DOTween.Kill("BoatRotLeft" + 1);
+                DOTween.Kill("BoatRotCenter" + 1);
+                _l_partsToTurn[i].transform.DOLocalRotate(rightTurn, voileSpeedRot).SetEase(Ease.OutQuart).SetId("BoatRotRight" + i);
+            }
+        }
+        else if (h < 0)
+        {
+            for (int i = 0; i < _l_partsToTurn.Count; i++)
+            {
+                DOTween.Kill("BoatRotRight" + 1);
+                DOTween.Kill("BoatRotCenter" + 1);
+                _l_partsToTurn[i].transform.DOLocalRotate(leftTurn, voileSpeedRot).SetEase(Ease.OutQuart).SetId("BoatRotLeft" + i);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _l_partsToTurn.Count; i++)
+            {
+                DOTween.Kill("BoatRotLeft" + 1);
+                DOTween.Kill("BoatRotRight" + 1);
+                _l_partsToTurn[i].transform.DOLocalRotate(Vector3.zero, voileSpeedRot).SetEase(Ease.OutQuart).SetId("BoatRotCenter" + i);
+            }
+        }
 	}
 }
