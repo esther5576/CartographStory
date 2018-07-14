da0152f326bde21925c66311819f083d4120ab6f
Kurtn <Kurtn77@gmail.com>
2018-07-14 17:02:33 +0200

    Push for jumping fish

---
+++ Assets/Scripts/JumpFishManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpFishManager : MonoBehaviour
+{
+    public ParticleSystem JumpFishParticleSystem;
+
+    public float ChanceToTrigger; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Random.Range(0.0f,1.0f) <= ChanceToTrigger)
+        {
+            if (!JumpFishParticleSystem.isPlaying)
+                JumpFishParticleSystem.Play();
+        }
+    }
+}
